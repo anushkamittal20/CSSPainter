1f383eb5dc90cf18154aa022cf30b08aed5a7909
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2020-10-08 19:22:46 +0530

    Added HTML Reader

---
+++ app.js
@@ -0,0 +1,8 @@
+
+function handleSubmit(e){
+    let inputValue=document.getElementById("input-form").value
+    console.log(inputValue)
+    document.getElementById("output").innerHTML = inputValue;
+    
+}
+
