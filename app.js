f473399ec0290a8e4a8184974305111cff7bc9a7
Pr1-xD <63440566+Pr1-xD@users.noreply.github.com>
2020-10-15 02:22:09 +0530

    Added Functionality

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 
+function showCssEditor()
+{   
+    let htmleditor=document.getElementById("htmlcontainer")
+    let csseditor=document.getElementById("csseditor")
+    htmleditor.classList.add("hide")
+    csseditor.classList.remove("hide")
+}
+function showHtmlEditor()
+{   
+    let htmleditor=document.getElementById("htmlcontainer")
+    let csseditor=document.getElementById("csseditor")
+    csseditor.classList.add("hide")
+    htmleditor .classList.remove("hide")
+}
+
 function handleSubmit(e){
-    let inputValue=document.getElementById("input-form").value
+    let inputValue=document.getElementById("input-form").value 
+    let outputBox=document.getElementById("output")
     console.log(inputValue)
-    document.getElementById("output").innerHTML = inputValue;
-    
+    outputBox.innerHTML = inputValue;
+    showCssEditor()
 }
 function elSubmit(e){
     let elVal=document.getElementById("element-name").value;
@@ -13,6 +29,5 @@ function elSubmit(e){
     { var x = el.nodeName;
         console.log(x);}
     else
-    alert('Not Found')    
+    alert('Not Found')
 }
-
